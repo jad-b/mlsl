8733fecc1aba4f60c8e05668a37d6438536ac076
Jeremy Dobbins-Bucklad <j.american.db@gmail.com>
2016-05-10 08:51:49 -0400

    Re-org graphlab loading

--- mlsl/glcreate.py
+++ mlsl/glcreate.py
@@ -9,24 +9,30 @@ Graphlab does not support Python 3. - jdb, 2016May09
 """
 import argparse
 import os
+import sys
 import time
 
-start = time.clock()  # noqa
-import graphlab
 
-gl_product_key = os.getenv('GLCREATE_PRODUCT_KEY',
-                           False)
-graphlab.product_key.set_product_key(gl_product_key)
-# Display graphlab canvas in notebook
-graphlab.canvas.set_target('ipynb')
-# Number of workers
-graphlab.set_runtime_config('GRAPHLAB_DEFAULT_NUM_PYLAMBDA_WORKERS', 16)
-since = time.clock() - start
-print("Graphlab loaded in {:.3f} seconds".format(since))
+def load_graphlab():
+    if sys.version_info >= (3, 0):
+        raise "Graphlab is only available in Python 2"
+    start = time.clock()  # noqa
+    import graphlab
+    gl_product_key = os.getenv('GLCREATE_PRODUCT_KEY',
+                               False)
+    graphlab.product_key.set_product_key(gl_product_key)
+    # Display graphlab canvas in notebook
+    graphlab.canvas.set_target('ipynb')
+    # Number of workers
+    graphlab.set_runtime_config('GRAPHLAB_DEFAULT_NUM_PYLAMBDA_WORKERS', 16)
+    since = time.clock() - start
+    print("Graphlab loaded in {:.3f} seconds".format(since))
+    return graphlab
 
 
 def convert_to_csv(filename):
-    sframe = graphlab.SFrame(filename)
+    gl = load_graphlab()
+    sframe = gl.SFrame(filename)
     noext_filename, _ = os.path.splitext(os.path.abspath(filename))
     new_filename = noext_filename + '.csv'
     df = sframe.to_dataframe()
@@ -37,6 +43,6 @@ def convert_to_csv(filename):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('file',
-                        help='GraphLab file to convert to numpy format')
+                        help='GraphLab file to convert to Pandas .csv')
     args = parser.parse_args()
     convert_to_csv(args.file)
