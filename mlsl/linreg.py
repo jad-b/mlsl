Fix batch gradient descent; refine utility tooling

mlsl/linreg.py
@@ -7,7 +7,7 @@ from scipy import linalg
 from sklearn.utils import shuffle
 
 from mlsl.log import log, perflog
-from mlsl.util import to_ndarray, add_bias_column
+from mlsl import util
 
 
 class LinearRegression:
@@ -35,20 +35,16 @@ class LinearRegression:
         """Fit the model to the data. Learns || updates model parameters."""
         # Allow the user to determine how we learn the model parameters.
         if fn is None:
-            fn = self.stochastic_gradient_descent
-        # Make sure we're using numpy n-dimensional arrays
-        X, y = to_ndarray(X), to_ndarray(y)
-        assert isinstance(X, np.ndarray)
-        assert isinstance(y, np.ndarray)
-        # Add a "bias" column of 1s. Since multiplying by 1 acts as a no-op, we
-        # can learn a parameter to describe the "bias" in the data.
-        X = add_bias_column(X)
+            fn = self.batch_gradient_descent
+        X, y = util.prepare_data_matrix(X), util.to_ndarray(y)
+        if y.ndim != 2 or y.shape[1] != 1:  # If y's not a column vector
+            y = y.reshape((len(y), 1))
         log.debug("Fitting model to data (%d samples x %d features)",
                   X.shape[0], X.shape[1])
 
-        start = time.clock()
-        self.weights = fn(X, y, **kwargs)
-        perflog.info("Fit model in %.3f seconds", time.clock() - start)
+        self.weights, metadata = fn(X, y, **kwargs)
+        if metadata:
+            log.info(util.format_metadata(metadata), metadata)
 
         return self.weights
 
@@ -73,7 +69,11 @@ class LinearRegression:
         return J, dJ
 
     def predict(self, X):
-        """Predict values of y from X, using our model."""
+        """Predict values of y from X, using our model.
+
+        .. math::
+            h_{\\theta}(x) = \\theta_{0} + \\theta_{1}x
+        """
         start = time.clock()
         h = X.dot(self.weights)
         perflog.info("Predicted %d values in %.3f seconds", X.shape[1],
@@ -82,6 +82,7 @@ class LinearRegression:
 
     def evaluate(self, X, y):
         """Determine accuracy of our model against labeled data (y)."""
+        X, y = util.prepare_data_matrix(X), util.to_ndarray(y)
         h = self.predict(X)
         assert len(h) == len(y)
         self.accuracy = (h == y).sum() / len(y)
@@ -104,27 +105,55 @@ class LinearRegression:
         # (X^T*X)^-1 * X^T
         Xt = linalg.inv(X.T.dot(X)).dot(X.T)
         # Multiply the pseudo-inverse by y
-        return Xt.dot(y)
+        return Xt.dot(y), None
 
     # Cost Function
-    def _least_squares(self, X, y):
-        """Find the cost using the least squares regression."""
-        # Our predictions
-        h = X.dot(self.weights)
-        # The error of our predictions, compared to the actual values
-        err = y - h
-        # The cost of our predictions
-        J = err.T.dot(err)
-        # The gradient||vector of partial derivatives
-        dJ = -2*X.T.dot(h)
-        return J, dJ
+    def _least_squares(self, X, y, lambda_=0, **kwargs):
+        """Find the cost using the least squares regression.
 
-    def batch_gradient_descent(self, X, y, alpha=.01, tolerance=1e-9, **kwargs):
+        .. math::
+            h_{\\theta}(x) = \\theta_{0} + \\theta_{1}x
+            J(\\theta) =
+                \\frac{1}{2m}\sum_{i=1}^m(h_{\\theta}(x^{i}) - y^{i})^{2}
+        """
+        assert X.ndim == 2, "X should be a matrix"
+        assert y.ndim == 2, "y should be a column vector"
+        assert y.shape[1] == 1, "y should be a column vector"
+        m = len(y)  # Number of samples
+        n = X.shape[1]  # Number of features
+        # J = The cost of our current model
+        #   = 1/(2m) * sum((y - Xw)^2)
+        #   = 1/(2m) * sum((predictions - actual)^2)
+        # Our predictions
+        predictions = X.dot(self.weights)
+        # The error of our predictions, compared to the actual values
+        error = predictions - y
+        # J = 1/(2m) * sum(error^2)
+        J = (1/(2*m)) * error.T.dot(error)
+        assert J.shape == (1, 1)
+        # dJ = The gradient||vector of partial derivatives
+        #    = 1/m * X'(y - Xw)
+        #    = 1/m * X'(error)
+        dJ = (1/m) * X.T.dot(error)
+        assert dJ.shape == self.weights.shape, "dJ should have the same dims as y"
+        return np.asscalar(J), dJ
+
+    # Learning function
+    def batch_gradient_descent(self, X, y, alpha=1e-3, maxiters=np.inf,
+            tolerance=1e-9, **kwargs):
+        """
+        .. math::
+            \\theta_{j} := \\alpha\\frac{1}{m}\sum_{i=1}^m
+                     (h_{\\theta}(x^{(i)})-y^{(i)})x_{j}^{(i)}
+        """
+        # A general default is to initialize weights to zero.
+        if self.weights is None:
+            self.weights = np.zeros((X.shape[1], 1))  # A column vector
+            log.info("Zero'd %d model parameters", len(self.weights))
+
         change = sys.float_info.max  # Largest possible Python float
-        if not self.weights:
-            self.weights = np.zeros(X.shape[1])
-        prevJ, iters, starttime = .0, 0, time.perf_counter()
-        while change > tolerance:  # Check for convergence
+        prevJ, iters, starttime = np.inf, 0, time.perf_counter()
+        while change > tolerance and iters < maxiters:  # Check for convergence
             # Find cost and partial derivatives for update
             J, dJ = self.cost(X, y)
             # Update each weight by a "step", its respective partial derivative.
@@ -132,50 +161,54 @@ class LinearRegression:
             # Multiplying by alpha, a fraction, reduces our chance of
             # overstepping and overshooting our target
             self.weights -= alpha * dJ
-            change = math.abs(prevJ - J)
-            log.debug("Change in cost: %.3e".format(change))
+            change = math.fabs(prevJ - J)
+            log.debug("Change in cost: %.3e", change)
             prevJ, iters = J, iters + 1
 
-        log.info("Converged w/ cost %.3e".format(J))
+        log.info("Converged w/ cost %.3e", J)
         return self.weights, {
             'cost': J,  # Final cost
             'iterations': iters,
-            'time': starttime - time.perf_counter()
+            'time': time.perf_counter() - starttime
         }
 
-    def stochastic_gradient_descent(self, X, y, alpha=.01, tolerance=1e-9,
-            **kwargs):
-        change = sys.float_info.max
-        if not self.weights:
-            self.weights = np.zeros(X.shape[1])
-        prevJ, iters, starttime = .0, 0, time.perf_counter()
-        log.info("Shuffling input...")
-        # Pandas DataFrame's let you randomly sample a fraction of its values.
-        # A fraction of 1 is the same as sampling 100% of all values.
-        X = shuffle(X)
+    # Learning function
+    def stochastic_gradient_descent(self, X, y, alpha=1e-3, maxiters=np.inf,
+            tolerance=1e-9, **kwargs):
+        # A general default is to initialize weights to zero.
+        if self.weights is None:
+            self.weights = np.zeros((X.shape[1], 1))  # A column vector
+            log.info("Zero'd %d model parameters", len(self.weights))
 
         # We can consider iteration through a randomly shuffled X a random
         # sampling from X
-        for x in X:
-            start = time.clock()
-            # Find cost and partial derivatives for update
-            J, dJ = self._least_squares(x, y)
-            # Update each weight by a "step", its respective partial derivative.
-            # Using a vector (numpy array) lets us update in parallel.
-            # Multiplying by alpha, a fraction, reduces our chance of
-            # overstepping and overshooting our target
-            self.weights -= alpha * dJ
-            change = math.fabs(prevJ - J)
-            log.debug("Change in cost: %.3e", change)
-            if change < tolerance:  # Check for convergence
-                break
-            prevJ, iters = J, iters + 1
-            perflog.debug("SGD iteration took %.3f seconds",
-                          time.clock() - start)
+        X, y = shuffle(X, y)
+        iters, starttime, = 0, time.perf_counter()
+        change, prevJ = sys.float_info.max, np.inf
+        while change > tolerance and iters < maxiters:
+            for i, x in enumerate(X):
+                # Find cost and partial derivatives for update
+                J, dJ = self.cost(
+                        x.reshape((1, x.shape[0])),  # => (1 x features) row vector
+                        y[i].reshape((1,1))  # => 1x1 matrix|column vector
+                        )
+                # assert J < prevJ, "Cost is increasing"
+                # Update each weight by a "step", its respective partial derivative.
+                # Using a vector (numpy array) lets us update in parallel.
+                # Multiplying by alpha, a fraction, reduces our chance of
+                # overstepping and thus overshooting our target
+                self.weights -= alpha * dJ
+                # Check for convergence
+                change = math.fabs(prevJ - J)
+                log.debug("Change in cost: %.3e", change)
+                if change < tolerance or iters >= maxiters:
+                    break
+                # Prepare for next iteration
+                prevJ, iters = J, iters + 1
 
         log.info("Converged w/ cost %.3e", J)
         return self.weights, {
             'cost': J,
             'iterations': iters,
-            'time': starttime - time.perf_counter()
+            'time': time.perf_counter() - starttime
         }

mlsl/tests/test_linreg.py
@@ -3,13 +3,14 @@ import textwrap
 import time
 from collections import namedtuple
 
+import numpy as np
 import pandas
 import pytest
 from sklearn.cross_validation import train_test_split
 
 from mlsl.linreg import LinearRegression
 from mlsl.log import testlog, perflog
-from mlsl import MLSL_DIR
+from mlsl import MLSL_DIR, util
 
 
 TrainTestSplit = namedtuple("TrainTestSplit", ["X_train", "X_test",
@@ -57,6 +58,80 @@ def dataset(request):
 
 def test_dataset_loading(dataset):
     pass
+
+
+def test_least_squares():
+    TC = namedtuple('TC', ['X', 'y', 'theta', 'cost', 'grad'])
+    testcases = [
+        TC(np.array([[1, 2], [1, 3], [1, 4], [1, 5]]),
+           np.array([[7,6,5,4]]).T,
+           np.array([[0.1, 0.2]]).T,
+           11.9450,
+           None),
+        TC(np.array([[1,2,3], [1,3,4], [1,4,5], [1,5,6]]),
+           np.array([[7,6,5,4]]).T,
+           np.array([[0.1, 0.2, 0.3]]).T,
+           7.0175,
+           None),
+    ]
+    for tc in testcases:
+        lr = LinearRegression()
+        lr.weights = tc.theta
+        cost, _ = lr._least_squares(tc.X, tc.y)
+        assert np.isclose(tc.cost, cost, 1e-3)
+
+
+@pytest.mark.skip()
+def test_regularized_least_squares():
+    TC = namedtuple('TC', ['X', 'y', 'theta', 'lambda_', 'cost', 'grad'])
+    testcases = [
+        TC(np.array([[1, 1, 1]]),
+           np.array([[7, 6, 5]]).T,
+           np.array([[.1, .2, .3, .4]]).T,
+           0,
+           1.3533,
+           np.array([[-1.4, -8.7333, -4.3333, -7.9333]]).T),
+        TC(np.array([[1, 2, 3, 4]]),
+           np.array([[5]].T),
+           np.array([[0.1, 0.2, 0.3, 0.4]]).T,
+           7,
+           3.015,
+           np.array([[-2., -2.6, -3.9, -5.2]]).T)
+    ]
+    for tc in testcases:
+        lr = LinearRegression()
+        lr.weights = tc.theta
+        cost, grad = lr._least_squares(tc.X, tc.y, lambda_=tc.lambda_)
+        assert tc.cost == cost
+        assert tc.grad == grad
+
+
+def test_gradient_descent():
+    TC = namedtuple('TC', ['X', 'y', 'theta', 'alpha', 'iters', 'weights'])
+    testcases = [
+        TC(np.array([[1, 5], [1, 2], [1, 4], [1, 5]], dtype=np.int64),
+           np.array([[1, 6, 4, 2]]).T,
+           np.array([[0., 0.]]).T,
+           0.01,
+           1000,
+           np.array([[5.2148, -0.5733]]).T),
+        TC(np.array([[1, 5], [1, 2]]),
+           np.array([[1, 6]]).T,
+           np.array([[.5, .5]]).T,
+           0.1,
+           10,
+           np.array([[1.70986, 0.19229]]).T)
+        ]
+    optimizers = (
+        LinearRegression.batch_gradient_descent,
+        LinearRegression.stochastic_gradient_descent
+    )
+    for minfn in optimizers:
+        for tc in testcases:
+            lr = LinearRegression()
+            lr.weights = tc.theta
+            w, meta = minfn(lr, tc.X, tc.y, alpha=tc.alpha, maxiters=tc.iters)
+            assert np.allclose(lr.weights, tc.weights, 1e-4)
 
 
 def test_linear_regression(dataset):

mlsl/util.py
@@ -1,7 +1,39 @@
 import math
+from io import StringIO
 
 import numpy as np
 import pandas
+
+from mlsl.log import log
+
+
+def prepare_data_matrix(X):
+    """Idempotently installs a bias column and converts to a numpy.ndarray."""
+    # numpy's ndarrays seem to be the simplest option for data operations.
+    # Pandas DataFrames have some more complicated behaviour when it comes to
+    # iteration.
+    npX = to_ndarray(X)
+    # Add a "bias" column of 1s. Since multiplying by 1 acts as a no-op, we
+    # can learn a parameter to describe the "bias" in the data.
+    return add_bias_column(npX)
+
+
+def to_ndarray(X):
+    """Convert *whatever* into a :class: `numpy.ndarray`, or die trying."""
+    ret = None
+    try:
+        if isinstance(X, np.ndarray):
+            ret = X
+        if isinstance(X, pandas.DataFrame) or isinstance(X, pandas.Series):
+            ret = X.values
+        else:
+            raise Exception(
+                    "Failed conversion to numpy.ndarray "
+                    "from unsupported type '{}'".format(type(X)))
+    finally:
+        if ret is not None:
+            assert isinstance(ret, np.ndarray) # Call me paranoid.
+            return ret
 
 
 def add_bias_column(X):
@@ -11,6 +43,58 @@ def add_bias_column(X):
         return np.c_[ones, X]
     else:
         return X
+
+
+def to_col_vec(x):
+    """Converts a row vector to a column vector.
+
+    If it's a 1D array, then the extra dimension is added.
+    If it's a matrix, an exception is raised.
+    """
+    if x.nimd == 2 and x.shape[1] == 1:
+        return x  # Already a column vector
+    if x.ndim < 2:  # 1D array
+        # Wrap in another array and return the tranpose
+        return np.array([x]).T
+    if x.shape[0] == 1:  # 2D row vector
+        return x.T
+    raise ValueError("""
+        Cannot convert _x_ to column vector.
+        Dimensions: {}
+        Shape: {}
+        x: {}
+    """.format(x.ndim, x.shape, x))
+
+
+def feature_normalize(X):
+    """Normalize the features of X."""
+    mu = X.mean(axis=1)    # Mean of each column (feature)
+    sigma = X.std(axis=1)  # Std. dev. of each feature
+    Xn = X- mu
+    Xn /= sigma
+    return Xn, mu, sigma
+
+
+def assert_shape(a, x):
+    assert a.shape == x, _obs_exp(a.shape, x)
+
+
+def format_metadata(meta):
+    """Prepares a dictionary of metadata for printing."""
+    sio = StringIO()
+    sio.write("\nMetadata\n========\n")
+    for k, v in sorted(meta.items()):
+        sio.write("{key} = %({key})".format(key=k))
+        if isinstance(v, float):
+            sio.write(".3f")
+        elif isinstance(v, int):
+            sio.write("d")
+        elif isinstance(v, str):
+            sio.write("s")
+        else:
+            log.warning("Unrecognized type: %s", type(v))
+        sio.write('\n')
+    return sio.getvalue()
 
 
 def print_shape(name, X):
@@ -23,43 +107,3 @@ def print_val(name, val):
 
 def _obs_exp(a, b):
     return "\nObserved: {}\nExpected: {}".format(a, b)
-
-
-def to_ndarray(X):
-    """Convert whatever into a :class: `numpy.ndarray`."""
-    if isinstance(X, np.ndarray):
-        return X
-    if isinstance(X, pandas.DataFrame) or isinstance(X, pandas.Series):
-        return X.values
-    else:
-        raise Exception(
-                "Failed conversion to numpy.ndarray "
-                "from unsupported type '{}'".format(type(X)))
-
-
-def assert_shape(a, x):
-    assert a.shape == x, _obs_exp(a.shape, x)
-
-
-def assertClose(a, b, tol):
-    assert np.allclose(a.flatten(), b.flatten(), atol=tol), \
-        _obs_exp(a, b)
-
-
-def to_col_vec(x):
-    """Converts a row vector to a column vector.
-
-    If it's a 1D array, then the extra dimension is added.
-    If it's a matrix, an exception is raised.
-    """
-    if x.ndim < 2:  # 1D array
-        # Wrap in another array and return the tranpose
-        return np.array([x]).T
-    if x.shape[0] == 1:  # 2D row vector
-        return x.T
-    raise ValueError("""
-        Cannot convert _x_ to column vector.
-        Dimensions: {}
-        Shape: {}
-        x: {}
-    """.format(x.ndim, x.shape, x))